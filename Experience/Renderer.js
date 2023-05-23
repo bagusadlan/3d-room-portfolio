d9459012b4137101f91cc9a1cc03e7ee0d2fe6e9
bagusadlan <bagusadlann@gmail.com>
2023-05-23 20:30:48 +0700

    add some utility function

---
+++ Experience/Renderer.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three'
+
+import Experience from "./Experience";
+
+export default class Remderer {
+  constructor() {
+    this.experience = new Experience()
+    this.sizes = this.experience.sizes
+    this.scene = this.experience.scene
+    this.canvas = this.experience.canvas
+    this.camera = this.experience.camera
+    
+    this.setRenderer()
+  }
+
+  setRenderer() {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true
+    })
+
+    this.renderer.useLegacyLights =  true
+    this.renderer.linearEncoding = THREE.LinearSRGBColorSpace
+    this.renderer.toneMapping = THREE.CineonToneMapping
+    this.renderer.toneMappingExposure = 1.75
+    this.renderer.shadowMap.enabled = true
+    this.renderer.shadowMap.type = THREE.PCFShadowMap
+    this.renderer.setSize(this.sizes.width, this.sizes.height)
+    this.renderer.setPixelRatio(this.sizes.pixelRation)
+  }
+
+  resize() {
+    this.renderer.setSize(this.sizes.width, THREE.sizes.height)
+    this.renderer.setPixelRatio(this.sizes.pixelRation)
+  }
+
+  update() {
+    this.renderer.render(this.scene, this.camera.perspectiveCamera)
+  }
+}
