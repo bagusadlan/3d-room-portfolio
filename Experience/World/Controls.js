cdb5b62f86facf07175ff308e711eca227c50e91
bagusadlan <bagusadlann@gmail.com>
2023-06-10 03:24:39 +0700

    add scrollTrigger to room object

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+import GSAP from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger.js'
 
 import Experience from "../Experience";
 
@@ -9,6 +11,24 @@ export default class Controls {
     this.resources = this.experience.resources
     this.time = this.experience.time
     this.camera = this.experience.camera
+    this.room = this.experience.world.room.actualRoom
+    GSAP.registerPlugin(ScrollTrigger)
+
+    this.setPath()
+  }
+
+  setPath() {
+    this.timeline = new GSAP.timeline()
+    this.timeline.to(this.room.position, {
+      x: 2.5,
+      scrollTrigger: {
+        trigger: ".first-move",
+        markers: true,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 0.8
+      }
+    })
   }
 
   resize() {
