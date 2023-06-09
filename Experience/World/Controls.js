52e42e9f317fde44d4a4f785bf59632f574e74c0
bagusadlan <bagusadlann@gmail.com>
2023-06-10 03:42:25 +0700

    fix scrollTrigger  when resize the screen

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -8,6 +8,7 @@ export default class Controls {
   constructor() {
     this.experience = new Experience()
     this.scene = this.experience.scene
+    this.sizes = this.experience.sizes
     this.resources = this.experience.resources
     this.time = this.experience.time
     this.camera = this.experience.camera
@@ -20,13 +21,16 @@ export default class Controls {
   setPath() {
     this.timeline = new GSAP.timeline()
     this.timeline.to(this.room.position, {
-      x: 2.5,
+      x: () => {
+        return this.sizes.width * 0.0016
+      },
       scrollTrigger: {
         trigger: ".first-move",
         markers: true,
         start: "top top",
         end: "bottom bottom",
-        scrub: 0.8
+        scrub: 0.8,
+        invalidateOnRefresh: true
       }
     })
   }
