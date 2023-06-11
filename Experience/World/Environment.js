61963d85516433cf4ce0ec8dac6618331ca83626
bagusadlan <bagusadlann@gmail.com>
2023-06-11 16:54:44 +0700

    remove gui and refactor some code

--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -1,8 +1,7 @@
 import * as THREE from 'three'
 import GSAP from 'gsap'
-import GUI from 'lil-gui'
 
-import Experience from "../Experience";
+import Experience from "../Experience"
 
 export default class Environment {
   constructor() {
@@ -10,18 +9,26 @@ export default class Environment {
     this.scene = this.experience.scene
     this.resources = this.experience.resources
 
-    this.gui = new GUI({ container: document.querySelector('.hero-main')})
-    this.obj = {
-      colorObj: {
-        r: 0,
-        g: 0,
-        b: 0
+    this.color = {
+      colorDark: {
+        r: 0.17254901960784313,
+        g: 0.23137254901960785,
+        b: 0.6862745098039216
       },
-      intensity: 1
+      intensityDark: {
+        intensity: 1
+      },
+      colorLight: {
+        r: 1,
+        g: 1,
+        b: 1
+      },
+      intensityLight: {
+        intensity: 1
+      }
     }
 
     this.setSunlight()
-    this.setGUI()
   }
 
   setSunlight() {
@@ -30,9 +37,6 @@ export default class Environment {
     this.sunLight.shadow.camera.far = 20
     this.sunLight.shadow.mapSize.set(2048, 2048)
     this.sunLight.shadow.normalBias = 0.05
-
-      // const helper = new THREE.CameraHelper(this.sunLight.shadow.camera)
-      // this.scene.add(helper)
     
     this.sunLight.position.set(1.5, 7, 3)
     this.scene.add(this.sunLight)
@@ -41,53 +45,17 @@ export default class Environment {
     this.scene.add(this.ambientLight)
   }
 
-  setGUI() {
-    this.gui.addColor(this.obj, "colorObj").onChange(() => {
-      this.sunLight.color.copy(this.obj.colorObj)
-      this.ambientLight.color.copy(this.obj.colorObj)
-      console.log(this.obj.colorObj);
-    })
-    this.gui.add(this.obj, "intensity").onChange(() => {
-      this.sunLight.intensity = this.obj.intensity
-      this.ambientLight.intensity = this.obj.intensity
-    })
-  }
-
   switchTheme(theme) {
     if (theme === "dark") {
-      GSAP.to(this.sunLight.color, {
-        r: 0.17254901960784313,
-        g: 0.23137254901960785,
-        b: 0.6862745098039216
-      })
-      GSAP.to(this.ambientLight.color, {
-        r: 0.17254901960784313,
-        g: 0.23137254901960785,
-        b: 0.6862745098039216
-      })
-      GSAP.to(this.sunLight, {
-        intensity: 1
-      })
-      GSAP.to(this.ambientLight, {
-        intensity: 1
-      })
+      GSAP.to(this.sunLight.color, this.color.colorDark)
+      GSAP.to(this.ambientLight.color, this.color.colorDark)
+      GSAP.to(this.sunLight, this.color.intensityDark)
+      GSAP.to(this.ambientLight, this.color.intensityDark)
     } else {
-      GSAP.to(this.sunLight.color, {
-        r: 1,
-        g: 1,
-        b: 1
-      })
-      GSAP.to(this.ambientLight.color, {
-        r: 1,
-        g: 1,
-        b: 1
-      })
-      GSAP.to(this.sunLight, {
-        intensity: 1
-      })
-      GSAP.to(this.ambientLight, {
-        intensity: 1
-      })
+      GSAP.to(this.sunLight.color, this.color.colorLight)
+      GSAP.to(this.ambientLight.color, this.color.colorLight)
+      GSAP.to(this.sunLight, this.color.intensityLight)
+      GSAP.to(this.ambientLight, this.color.intensityLight)
     }
   }
 
